9f36e469558203441432a7de0c976dee0722eed1
Patrick McGuire <pjm2119@columbia.edu>
2014-04-21 20:13:50 -0400

    there's a snake in my boots

---
+++ snake.js
@@ -0,0 +1,126 @@
+// <- 0 ^ 1 -> 2 down 3
+var LEFT = 0;
+var UP = 1;
+var RIGHT = 2;
+var DOWN = 3;
+
+var min = 0;
+var max = 500;
+
+var snakelets = [{
+  x: Math.floor((max / 2) / 10) * 10,
+  y: Math.floor((max / 2) / 10) * 10
+}];
+var foods = {};
+
+function clamp(val) {
+  if (val < min) {
+    return max;
+  } else if (val >= max) {
+    return min;
+  } else {
+    return val;
+  }
+}
+
+// size 10x10
+function moveSnake(dir) {
+  var slast = snakelets.pop();
+  if (snakelets.length > 0) {
+    var first = snakelets[0];
+  } else {
+    var first = slast;
+  }
+
+  s = {};
+  s.x = first.x;
+  s.y = first.y;
+  switch(dir) {
+    case LEFT:
+      s.x = clamp(s.x - 10);
+      break;
+    case UP:
+      s.y = clamp(s.y - 10);
+      break;
+    case DOWN:
+      s.y = clamp(s.y + 10);
+      break;
+    case RIGHT:
+      s.x = clamp(s.x + 10);
+      break;
+  }
+  snakelets.unshift(s);
+  checkFood(slast);
+  checkDeath();
+  redraw();
+}
+
+function checkFood(slast) {
+  var snakelet = snakelets[0];
+  var foodkey = JSON.stringify({x: snakelet.x, y: snakelet.y});
+  if (foodkey in foods) {
+    delete foods[foodkey];
+    snakelets.push({x: slast.x, y: slast.y});
+  }
+}
+
+
+function getRandomInt (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function checkDeath() {
+
+}
+
+
+function redraw() {
+  var c = document.getElementById('mycanvas');
+  var ctx = c.getContext("2d");
+  ctx.clearRect(min, min, max, max)
+  ctx.fillStyle = "#000000";
+  for (i in snakelets) {
+    var snakelet = snakelets[i];
+    ctx.fillRect(snakelet.x,snakelet.y,9,9);
+  }
+
+  ctx.fillStyle = "#00FF00";
+  for (indices in foods) {
+    var loc = JSON.parse(indices);
+    ctx.fillRect(loc.x,loc.y,9,9);
+  }
+}
+
+var dir = LEFT;
+
+for (var i = 0; i < 100; i++) {
+  var foodLoc = {};
+  foodLoc.x = Math.floor(getRandomInt(min,max) / 10) * 10;
+  foodLoc.y = Math.floor(getRandomInt(min,max) / 10) * 10;
+  foods[JSON.stringify(foodLoc)] = foodLoc;
+  1 / 0;
+}
+
+setInterval(function() {
+  moveSnake(dir);
+}, 100);
+
+
+window.onkeydown = function(e) {
+  console.log(e);
+  switch (e.keyCode) {
+  case 65:
+    dir = LEFT;
+    break;
+  case 87:
+    dir = UP;
+    break;
+  case 68:
+    dir = RIGHT;
+    break;
+  case 83:
+    dir = DOWN;
+    break;
+  } 
+};
+
